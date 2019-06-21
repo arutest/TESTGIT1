08c361ff18655ab8c47f0a715f0dbe2b953c1ac4
Andres Rojas <arutestu@hotmail.com>
2019-06-21 17:21:23 -0400

    Primer envio general

---
+++ Default.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace WebAruTest1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "srv-arutest-cl.database.windows.net";
+                builder.UserID = "arojas";
+                builder.Password = "aruazure.77";
+                builder.InitialCatalog = "ARUTEST1";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    //Console.WriteLine("\nQuery data example:");
+                    //Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT *  ");
+                    sb.Append("FROM [TEST1] T ");
+                    //sb.Append("JOIN [SalesLT].[Product] p ");
+                    //sb.Append("ON pc.productcategoryid = p.productcategoryid;");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                                Text1.Value = reader.GetValue(0).ToString();
+                                Text2.Value = reader.GetValue(1).ToString();
+                                Text3.Value = reader.GetValue(3).ToString();
+
+                            }
+                        }
+                    }
+                }
+            }
+            finally {
+
+            }
+            /*catch (SqlException e)
+            {
+                //Console.WriteLine(e.ToString());
+            }*/
+
+        }
+    }
+}
---
+++ obj/Release/Package/PackageTmp/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebAruTest1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>PRUEBA DE .NET en AZURE</h1>
+        <p class="lead">Esta es una página de prueba de -NET para instalarlo en AZure.</p>
+        <p><a href="#" class="btn btn-primary btn-lg">Aprenda mas y mas.. &raquo;</a></p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Comenzando ....</h2>
+            <p>
+                Aqui va un texto adicional mas largo........ la idea es probar esta herramienta .....            </p>
+            <p>
+                <a class="btn btn-default" href="#">Aprenda más &raquo;</a>
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>>Obtenga más libraries</h2>
+            <p>
+                NuGet es gratis como herramienta de VS Code (Traducido por ARU) Visual Studio projects.
+            </p>
+            <p>
+                <a class="btn btn-default" href="V">Aprenda más &raquo;</a>
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>Datos de BD</h2>
+            <p>
+                Datos obtenidos de BD Azure<br /><input runat="server" type="text" id="Text1" value=""><br />
+                <input runat="server" type="text" id="Text2" value=""><br /><input runat="server" type="text" id="Text3" value="">
+            </p>
+            <p>
+                <a class="btn btn-default" href="#">Learn more &raquo;</a>
+            </p>
+        </div>
+    </div>
+
+</asp:Content>
